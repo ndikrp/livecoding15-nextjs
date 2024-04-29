4c537978753c8560b907a2d949b6634a2aab80eb
ndikrp <andhikarizky36@gmail.com>
2024-04-29 20:16:36 +0700

    prop shielder, link to

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -1,4 +1,11 @@
-export default function AboutUsPage() {
-    return <h1>FSW 1 Terbaik</h1>;
-}
+import Link  from "next/link";
 
+export default function AboutUsPage() {
+  return (
+    <>
+      <h1 className="mb-10">FSW 1 Terbaik</h1>;
+      <Link href="/login">LOGIN LINK</Link>
+      <a href="/login" className="bg-purple-400 border p-12">Submit</a>
+    </>
+  );
+}
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,7 +3,7 @@ export default function LoginPage() {
     <form action="" className="bg-yellow-300">
       <input type="text" className="border-solid mr-3" />
       <input type="text" />
-      <button className="border-r-8 bg-blue-600"></button>
+      <button className="border-r-8 border-blue-600">Submit</button>
     </form>
   );
 }
